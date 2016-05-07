55a159a9c0f7d40a205ec80f811ff629540b45bf
pavelsof <pavelsof@gmail.com>
2016-05-07 17:40:36 +0200

    Front of front end ready for building upon.

--- app/scripts/ui/components.js
+++ app/scripts/ui/components.js
@@ -19,14 +19,29 @@ fa.ui.components = (function() {
 	Vue.component('search-view', {
 		template: '#search-view',
 		data: function() {
-			return {greeting: 'I am search, the searcher of worlds'};
+			return {
+				films: [
+					{id: 42, title: "The Touch of Stella's Love", year: 1910},
+					{id: 42, title: "Stella and Her Twelve Lovers", year: 1925},
+					{id: 42, title: "Stella's Smile", year: 1938},
+					{id: 42, title: "Who Can Compete with Stella?", year: 1966},
+					{id: 42, title: "Stella, It is a Love Story!", year: 1978},
+					{id: 42, title: "Galaxy 3: Battle for Stella", year: 1991},
+					{id: 42, title: "Stella is Happy", year: 2008}
+				],
+				friends: []
+			};
 		}
 	});
 	
 	Vue.component('film-view', {
 		template: '#film-view',
 		data: function() {
-			return {greeting: 'I am a film, trust me'};
+			return {
+				id: 42,
+				title: "Stella is Happy",
+				year: 2008
+			};
 		},
 		activate: function(done) {
 			console.log(this.$parent.routeParam);
