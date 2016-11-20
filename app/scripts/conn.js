0a8129319c304234ec044ff3b0593116bb689c2e
pavelsof <pavelsof@gmail.com>
2016-11-20 16:58:40 +0100

    Re-wrote conn.

--- app/scripts/conn.js
+++ app/scripts/conn.js
@@ -1,103 +1,90 @@
-/**
- * Handles the connection to the API.
- */
 fa.conn = (function() {
 	
 	"use strict";
 	
+	// 
+	// functions
+	// 
 	
-	/**
-	 * API calls
-	 * 
-	 * Return Promises that resolve into the JSON data load or reject with
-	 * {code, message}.
-	 */
-	var request = function(method, url, load) {
-		var headers = {};
-		var body = null;
+	// returns {method, headers, body}, ready to be plugged into fetch
+	// helper for the request function
+	var getOptions = function(method, load) {
+		var op = {'method': method, 'headers': {}};
+		
+		if(fa.auth.hasPerm()) {
+			op['headers']['fa-token'] = fa.auth.getToken();
+		}
 		
 		if(method == 'POST' || method == 'PUT') {
-			headers['content-type'] = 'application/json';
-			body = JSON.stringify(load);
-		}
-		if(fa.auth.getToken()) {
-			headers['fa-token'] = fa.auth.getToken();
+			op['headers']['content-type'] = 'application/json';
+			op['body'] = JSON.stringify(load);
 		}
 		
+		return op;
+	};
+	
+	// returns promise that resolves for 200 and rejects otherwise
+	// helper for the request function
+	var handleResponse = function(response) {
 		return new Promise(function(resolve, reject) {
-			fetch(fa.settings.API_URL+url, {
-				method: method,
-				headers: headers,
-				body: body
-			})
-			.then(function(response) {
-				response.json()
-				.then(function(data) {
-					if(response.ok)
-						resolve(data);
-					else if('error' in data)
-						reject({code: response.status, message: data['error']});
-					else
-						reject({code: response.status, message: response.statusText});
-				})
-				.catch(function(error) {
-					if(response.ok) resolve({});
-					else
-						reject({code: response.status, message: response.statusText});
+			var ctype = response.headers.get('content-type');
+			
+			if(ctype && ctype.indexOf('application/json') >= 0) {
+				response.json().then(function(data) {
+					if(response.ok) resolve(data);
+					else reject({
+						code: response.status,
+						message: data.error
+					});
+				}).catch(function(error) {
+					reject({code: response.status, message: error.message});
 				});
-			})
-			.catch(function(error) {
-				fa.logs.error(error);
+			}
+			else {
+				if(response.ok) resolve();
+				else reject({
+					code: response.status,
+					message: response.statusText
+				});
+			}
+		});
+	};
+	
+	// wrapper around fetch
+	// returns promise that resolves into the JSON data (if such) or rejects
+	// with {code, message}
+	var request = function(method, path, load) {
+		var url = fa.settings.API_URL + path;
+		var options = getOptions(method, load);
+		
+		return new Promise(function(resolve, reject) {
+			fetch(url, options).then(function(response) {
+				handleResponse(response).then(resolve).catch(reject);
+			}).catch(function(error) {  // http request could not be fulfilled
 				reject({code: null, message: error.message});
 			});
 		});
 	};
 	
-	var get = function(url) {
+	
+	// 
+	// exports
+	// 
+	
+	var api = {};
+	
+	api.get = function(url) {
 		return request('GET', url);
 	};
 	
-	var post = function(url, load) {
+	api.post = function(url, load) {
 		return request('POST', url, load);
 	};
 	
-	var put = function(url, load) {
+	api.put = function(url, load) {
 		return request('PUT', url, load);
 	};
 	
-	
-	/**
-	 * Checks whether the app is online.
-	 */
-	var online = function() {
-		return true;
-	};
-	
-	
-	/**
-	 * Module turn on/off functions
-	 */
-	var init = function() {
-		return;
-	};
-	
-	var destroy = function() {
-		return;
-	};
-	
-	
-	/**
-	 * Exports
-	 */
-	return {
-		init: init,
-		destroy: destroy,
-		
-		online: online,
-		
-		get: get,
-		post: post,
-		put: put
-	};
+	return api;
 	
 }());
