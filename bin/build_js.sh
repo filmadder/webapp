3dd494de361ad137a5232b976b5bec17cd19456a
pavelsof <pavelsof@gmail.com>
2016-12-17 13:29:30 +0100

    fa.ws is born.

--- bin/build_js.sh
+++ bin/build_js.sh
@@ -4,6 +4,7 @@ app/scripts/_init.js \
 app/scripts/logs.js \
 app/scripts/assert.js \
 app/scripts/conn.js \
+app/scripts/ws.js \
 app/scripts/films.js \
 app/scripts/users.js \
 app/scripts/auth.js \
