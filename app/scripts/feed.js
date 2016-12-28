079b3bfb933e7ea31768deb5b206ca6ca8d612ba
pavelsof <pavelsof@gmail.com>
2016-12-28 16:16:54 +0100

    The feed view is now operational.

--- app/scripts/feed.js
+++ app/scripts/feed.js
@@ -7,9 +7,53 @@ fa.feed = (function() {
 	// functions
 	// 
 	
+	// constructs and returns a feed item object from the backend data
+	var createItem = function(data) {
+		var item = {
+			created: data.created
+		};
+		
+		item.type = {
+			addedToWatched: (data.type == 'a'),
+			addedToWatchlist: (data.type == 'u'),
+			becameFriends: (data.type == 'f')
+		};
+		
+		if(item.type.addedToWatched || item.type.addedToWatchlist) {
+			item.user = data.user;
+			item.film = data.film;
+		}
+		else if(item.type.becameFriends) {
+			item.userA = data.user_a;
+			item.userB = data.user_b;
+		}
+		
+		return item;
+	};
+	
+	// constructs and returns a feed object from the backend-provided data
+	var createFeed = function(data) {
+		var feed = {};
+		var page = 0;
+		
+		feed.firstItems = fjs.map(createItem, data.items);
+		
+		// resolves into a list of feed items
+		feed.loadMore = function() {
+			page += 1;
+			return fa.ws.send('get_feed', {page: page}).then(function(data) {
+				return Promise.resolve(fjs.map(createItem, data.items));
+			});
+		};
+		
+		return feed;
+	};
+	
 	// returns promise that resolves into a feed object
-	var getPage = function(page) {
-		return fa.ws.send('get_feed', {page: page});
+	var getFeed = function() {
+		return fa.ws.send('get_feed', {page: 0}).then(function(data) {
+			return Promise.resolve(createFeed(data));
+		});
 	};
 	
 	
@@ -18,7 +62,7 @@ fa.feed = (function() {
 	// 
 	
 	return {
-		getPage: getPage
+		get: getFeed
 	};
 	
 }());
