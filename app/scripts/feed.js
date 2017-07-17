8e58b1dd117debc544cc7305deab06c9ca600853
pavelsof <pavelsof@gmail.com>
2017-07-17 15:11:35 +0200

    New feed item type: addedToWatching.

--- app/scripts/feed.js
+++ app/scripts/feed.js
@@ -1,12 +1,12 @@
 fa.feed = (function() {
-	
+
 	"use strict";
-	
-	
+
+
 	// 
 	// unpacking
 	// 
-	
+
 	// creates a downstream feed item film object from an upstream json object
 	// 
 	// the feed item film objects contain only the fields: pk, title, posterUrl 
@@ -18,22 +18,23 @@ fa.feed = (function() {
 			posterUrl: data.poster_url
 		};
 	};
-	
+
 	// creates a downstream feed item object from an upstream json object
 	var unpackItem = function(data) {
 		var item = {
 			created: fa.utils.humaniseTime(data.created)
 		};
-		
+
 		item.type = {
 			addedToWatched: (data.type == 'a'),
 			addedToWatchlist: (data.type == 'u'),
 			movedToWatchlist: (data.type == 'o'),
+			addedToWatching: (data.type == 'i'),
 			becameFriends: (data.type == 'f'),
 			wroteComment: (data.type == 'c'),
 			addedTags: (data.type == 't')
 		};
-		
+
 		if(item.type.becameFriends) {
 			item.userA = fa.users.unpackUser(data.user_a);
 			item.userB = fa.users.unpackUser(data.user_b);
@@ -45,10 +46,10 @@ fa.feed = (function() {
 				item.tags = data.tags;
 			}
 		}
-		
+
 		return item;
 	};
-	
+
 	// creates a downstream feed object from an upstream json object
 	// 
 	// the page var keeps track of how many pages have been already loaded; it
@@ -56,9 +57,9 @@ fa.feed = (function() {
 	var createFeed = function(data) {
 		var feed = {};
 		var page = Math.ceil(data.items.length / 20) - 1;
-		
+
 		feed.firstItems = fjs.map(unpackItem, data.items);
-		
+
 		// resolves into a list of feed items
 		feed.loadMore = function() {
 			page += 1;
@@ -66,41 +67,41 @@ fa.feed = (function() {
 				return Promise.resolve(fjs.map(unpackItem, data.items));
 			});
 		};
-		
+
 		// returns the number of pages that have been loaded so far
 		feed.getNumPages = function() {
 			return page + 1;
 		};
-		
+
 		return feed;
 	};
-	
-	
+
+
 	// 
 	// downstream api
 	// 
-	
+
 	// returns promise that resolves into a feed object
 	// 
 	// if given, the argument specifies the number of pages that the feed will
 	// start with; by default a single page of feed items will be loaded
 	var getFeed = function(pages) {
 		var perPage = (pages) ? 20 * pages : 20;
-		
+
 		return fa.ws.send('get_feed', {page: 0, per_page: perPage}).then(function(data) {
 			return Promise.resolve(createFeed(data));
 		});
 	};
-	
-	
+
+
 	// 
 	// exports
 	// 
-	
+
 	return {
 		unpackItem: unpackItem,
-		
+
 		get: getFeed
 	};
-	
+
 }());
