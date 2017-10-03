bdb303bf94acba16a6380264d9edce3a8647ac76
stela <stella.plamenova@gmail.com>
2017-10-03 12:35:27 +0200

    watch.sh updated

--- bin/watch.sh
+++ bin/watch.sh
@@ -14,6 +14,6 @@ bin/build_vendor.sh
 
 concurrently --raw --kill-others \
 	"chokidar 'app/templates/**/*.html' -c 'bin/build_html.sh'" \
-	"chokidar 'app/styles/*.less' -c 'bin/build_css.sh'" \
+	"chokidar 'app/styles/**/*.less' -c 'bin/build_css.sh'" \
 	"chokidar 'app/scripts/**/*.js' -c 'bin/build_js.sh'" \
 	"ecstatic --root build --port 9000 --cache 0"
