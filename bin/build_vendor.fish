98d1245364c30a101c0b932abe2d18b264ecbb02
pavelsof <pavelsof@gmail.com>
2019-04-04 22:32:32 +0200

    Updated most updateable dependencies.

--- bin/build_vendor.fish
+++ bin/build_vendor.fish
@@ -1,16 +1,14 @@
 #!/usr/bin/fish
 
-begin
-	cat \
-	node_modules/loglevel/dist/loglevel.min.js \
-	node_modules/signals/dist/signals.min.js \
-	node_modules/hasher/dist/js/hasher.min.js \
-	node_modules/crossroads/dist/crossroads.min.js \
-	node_modules/mustache/mustache.min.js \
-	node_modules/whatwg-fetch/fetch.js \
-	node_modules/functional.js/functional.js \
-	node_modules/datetime/build/datetime.js \
-	node_modules/hier/src/hier.js \
-	node_modules/hammerjs/hammer.min.js \
-	> build/scripts/vendor.js
-end
+cat \
+node_modules/loglevel/dist/loglevel.min.js \
+node_modules/signals/dist/signals.min.js \
+node_modules/hasher/dist/js/hasher.min.js \
+node_modules/crossroads/dist/crossroads.min.js \
+node_modules/mustache/mustache.min.js \
+node_modules/whatwg-fetch/fetch.js \
+node_modules/functional.js/functional.js \
+node_modules/pythonic-datetime/build/datetime.js \
+node_modules/hier/src/hier.js \
+node_modules/hammerjs/hammer.min.js \
+> build/scripts/vendor.js
